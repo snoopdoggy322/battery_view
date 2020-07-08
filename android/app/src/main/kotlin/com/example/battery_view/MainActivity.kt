364227c6f163c162a247a3186a8b43047c048569
snoopdoggy322 <38835822+snoopdoggy322@users.noreply.github.com>
2020-07-08 16:53:18 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/battery_view/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.battery_view
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:wave/wave.dart';
+import 'package:wave/config.dart';
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'test',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Battery'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static const platform = const MethodChannel('samples.flutter.dev/battery');
+  // Get battery level.
+  int _batteryLevel = 100;
+  Future<void> _getBatteryLevel() async {
+    int batteryLevel;
+    String batteryLevelText;
+    try {
+      final int result = await platform.invokeMethod('getBatteryLevel');
+      batteryLevel=result;
+    } on PlatformException catch (e) {
+      batteryLevelText = "Failed to get battery level: '${e.message}'.";
+    }
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    _getBatteryLevel();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+          Text(_batteryLevel.toString()+'%'),
+      Container(
+        width: MediaQuery.of(context).size.width*0.40,
+        height: MediaQuery.of(context).size.height*0.60,
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.black,
+            width: 8,
+          ),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: WaveWidget(
+            duration:21,
+            config: CustomConfig(
+              gradients: [
+                if(_batteryLevel<=100&&_batteryLevel>60)
+                [Colors.green, Colors.amberAccent],
+                [Colors.green, Colors.amberAccent],
+                [Colors.amberAccent, Colors.lightGreen],
+                [Colors.green, Colors.lightGreen],
+                if(_batteryLevel<=60&&_batteryLevel>20)
+                [Colors.orange, Colors.amberAccent],
+                if(_batteryLevel<=20&&_batteryLevel>0)
+                  [Colors.red, Colors.red],
+              ],
+              durations: [35000, 19440, 10800, 6000],
+              heightPercentages:
+              [1-_batteryLevel/100, 1-_batteryLevel/100,
+                1-_batteryLevel/100, 1-_batteryLevel/100],
+              blur: MaskFilter.blur(BlurStyle.inner, 5),
+              gradientBegin: Alignment.centerLeft,
+              gradientEnd: Alignment.centerRight,
+            ),
+            waveAmplitude: 1,
+            backgroundColor: Colors.transparent,
+            size: Size(double.infinity, double.infinity)
+
+        ),
+      ),
+          ],
+        ),
+      ),
+    );
+  }
+}
